232513f9a581ff99327d2af871101edd8d8cd226
Rahul <rahulneupane0090@gmail.com>
2024-06-24 15:46:35 +0545

    arrays & hashes

---
+++ Learning RUBY/arrays.rb
@@ -0,0 +1,21 @@
+# hobbies = Array.new
+# hobbies[0] = 'biking'
+# hobbies[3] = 'rubiks cube'
+# puts hobbies
+
+friends = Array["Kevin", "Karen", "Oscar","Airen"]
+# puts friends[-1]
+# puts
+# puts friends[1,2] # from , count
+# puts
+# friends[1] = "Mohini"
+# puts friends[0..2] # from , to
+
+
+puts friends.length
+puts friends.include? "Michael"
+# puts friends.reverse
+# puts friends.sort
+
+
+
---
+++ Learning RUBY/hashes.rb
@@ -0,0 +1,10 @@
+cities = {
+    'Kathmandu' => 'KTM',
+    'Lalitpur' => 'LTP',
+    :Bhaktapur => 'BKT',
+    3=> 'Three'
+}
+
+puts cities['Kathmandu']
+puts cities[:Bhaktapur]
+puts cities[3]
