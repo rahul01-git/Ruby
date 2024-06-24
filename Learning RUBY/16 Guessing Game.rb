c644b66cd474a111d610a007bae0a6fc78893a03
Rahul <rahulneupane0090@gmail.com>
2024-06-24 16:45:00 +0545

    guessing game

---
+++ Learning RUBY/16 Guessing Game.rb
@@ -0,0 +1,22 @@
+secret_num = rand(1..10).to_s
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+while guess != secret_num and !out_of_guesses
+    if guess_count < guess_limit
+        print "Enter your guess (#{guess_limit - guess_count} guess left): "
+        guess = gets.chomp
+        guess_count+=1
+    else
+        out_of_guesses = true
+    end
+end
+
+if out_of_guesses
+    puts "You Lose! 😔"
+else
+    puts "You won! 🏆"
+end
+puts ("The num was: " + secret_num.to_s)
