8741825c5adef1958d29a2aa7c76cd6dd5dcbd09
Rahul <rahulneupane0090@gmail.com>
2024-06-30 21:10:08 +0545

    letter frequency

---
+++ projects/7 letter frequency.rb
@@ -0,0 +1,27 @@
+def letter_frequency(text)
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a
+    frequency = {}
+    text.each_char do |char|
+        if chars.include?(char)
+            unless frequency.key?(char)
+                frequency[char] = 1
+            else
+                frequency[char] += 1
+            end
+
+        end
+    end
+
+    return frequency
+end
+
+
+puts "Letter Frequency"
+print "Text: "
+text = gets.chomp
+
+result = letter_frequency(text.upcase).sort
+
+for key,value in result
+    puts "#{key} = #{value}"
+end
---
+++ projects/hash/index.rb
@@ -0,0 +1,19 @@
+person = {:name => "John", :lastname => "Doe", :city => "KTM"}
+
+
+# print person.keys
+# print person.values
+
+
+# traversing hash
+# method 1
+# for key,value in person
+#     puts "#{key} = #{value}"
+# end
+
+# method 2 
+# i = 0
+# while i < person.length
+#     puts "#{person.keys[i]} = #{person.values[i]}"
+#     i+=1
+# end
